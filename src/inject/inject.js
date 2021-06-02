62c0e43ba569f2030043b5166dd88230e437f831
Marcos Beirigo <marcosbeirigo@gmail.com>
2021-06-02 18:52:01 -0500

    Git backup

---
+++ src/inject/inject.js
@@ -0,0 +1,78 @@
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+
+      if (window.location.href != "https://futemax.gratis/") {
+        removeRelated();
+        removeComments()
+        removeTitles();
+      }
+      awesomeHeader();
+      removeIframes();
+      setupMutationObserver(document);
+
+    }
+  }, 10);
+});
+
+function removeRelated() {
+  const related = document.getElementsByClassName("item-wd");
+
+  if (related && related.length > 0) {
+    related[0].parentElement.remove();
+  }
+}
+
+function removeComments() {
+  const comments = document.getElementsByClassName("fb-comments");
+
+  if (comments && comments.length > 0) {
+    comments[0].parentElement.remove();
+  }
+}
+
+function removeTitles() {
+  const titles = document.getElementsByClassName("title-widget");
+
+  if (titles && titles.length > 0) {
+    Array.from(titles).forEach(t => t.remove());
+  }
+}
+
+function awesomeHeader() {
+  const header = document.querySelector(".header .logo");
+  header.innerHTML = "<a class='travahomelink' href='/'><h1 class='travatitle'>SUPER TRAVAMAX PLUS</h1></a>";
+  header.className = "";
+}
+
+function removeIframes() {
+  const iframes = document.querySelectorAll("body ~ iframe");
+  if (iframes && iframes.length > 0) {
+    iframes.forEach(f => {
+      f.style.display = "none";
+      f.style.zIndex = -99999999;
+    });
+  }
+}
+
+function setupMutationObserver(element) {
+  let observer = new MutationObserver(mutations => {
+    for (let mutation of mutations) {
+      for (let addedNode of mutation.addedNodes) {
+
+        if (addedNode.nodeName === "IFRAME") {
+          if (addedNode.getAttribute("name") != "myFrameView") {
+            addedNode.style.display = "none";
+            addedNode.style.zIndex = -99999999;
+          } else {
+            addedNode.addEventListener('load', () => { setupMutationObserver(addedNode.contentWindow.document.body) }, true);
+          }
+
+        }
+      }
+    }
+  });
+  observer.observe(element, { childList: true, subtree: true });
+
+}
